f488b4af48a0a055e87ba536f78af8fcfd022ecc
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-05-21 15:25:51 +0100

    using logger from library

--- clients/elasticsearch/es_client.go
+++ clients/elasticsearch/es_client.go
@@ -3,7 +3,7 @@ package elasticsearch
 import (
 	"context"
 	"fmt"
-	"github.com/alessandroarosio/bookstore_items-api/logger"
+	"github.com/alessandroarosio/bookstore-utils-go/logger"
 	"github.com/olivere/elastic"
 	"time"
 )
@@ -22,11 +22,12 @@ type esClient struct {
 }
 
 func Init() {
+	log := logger.GetLogger()
 	client, err := elastic.NewClient(
 		elastic.SetURL("http://127.0.0.1:9200"),
 		elastic.SetHealthcheckInterval(10*time.Second),
-		//elastic.SetErrorLog(log.New(os.Stderr, "ELASTIC ", log.LstdFlags)),
-		//elastic.SetInfoLog(log.New(os.Stdout, "", log.LstdFlags)),
+		elastic.SetErrorLog(log),
+		elastic.SetInfoLog(log),
 	)
 
 	if err != nil {
